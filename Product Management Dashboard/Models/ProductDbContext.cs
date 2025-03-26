ab2d53fcff53d4f554850d4ffdd148131921acf8
Olatunde Haroun <haroun.ola@gmail.com>
2025-03-26 16:55:21 +0000

    Add ProductDbContext for EF Core database operations
    Introduce ProductDbContext class in Product_Management_Dashboard.Models.
    This class inherits from DbContext and is configured to use Entity
    Framework Core for database operations. The constructor accepts
    DbContextOptions<ProductDbContext> and passes it to the base class
    constructor. A DbSet<Product> property named Product is added to
    represent the collection of Product entities in the database. The
    OnConfiguring method is overridden to configure the context to use a
    SQL Server database with the specified connection string.

---
+++ Product Management Dashboard/Models/ProductDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Product_Management_Dashboard.Models
+{
+    public class ProductDbContext: DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Product { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSqlLocalDb;Initial Catalog=ProductManagement; Trusted_Connection=True");
+        }
+    }   
+}
